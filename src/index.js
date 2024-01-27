3dd4d7bf3c331a82fa32888df9857baf1846e419
imlaamer <lolitayenik@ukr.net>
2024-01-27 05:08:23 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from 'components/App';
 import './index.css';
+import App from 'components/App';
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
